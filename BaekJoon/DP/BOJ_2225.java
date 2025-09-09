7f329df9352ba97a923931ba7e0e4b1c6180f97d
asungkim <asungkim007@naver.com>
2025-09-09 13:37:28 +0900

    BOJ 2225 풀이

---
+++ BaekJoon/DP/BOJ_2225.java
@@ -0,0 +1,32 @@
+package DP;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_2225 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        final int MOD = 1_000_000_000;
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][1] = 1;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            dp[1][i] = i;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= k; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+            }
+        }
+
+        System.out.println(dp[n][k]);
+    }
+}
