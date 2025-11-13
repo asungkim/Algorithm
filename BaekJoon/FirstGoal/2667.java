5328c5d70c256674a13ba8b665ef9955d19bf4b4
asungkim <asungkim007@naver.com>
2025-11-13 11:46:21 +0900

    BOJ 2667 DFS 풀이 추가

--- BaekJoon/FirstGoal/2667.java
+++ BaekJoon/FirstGoal/2667.java
@@ -6,29 +6,32 @@ class Main {
     static int n;
     static int[][] map;
     static boolean[][] visited;
-    static int[] dx={-1,0,1,0};
-    static int[] dy={0,-1,0,1};
-
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, -1, 0, 1 };
+    static int cnt = 1;
 
     public static void main(String[] args) throws IOException {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        n=sc.nextInt();
-        map=new int[n][n];
-        visited=new boolean[n][n];
-        
-        for (int i=0;i<n;i++) {
-            char[] tmp=sc.next().toCharArray();
-            for (int j=0;j<n;j++) {
-                map[i][j]=tmp[j]-'0';
+        n = sc.nextInt();
+        map = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            char[] tmp = sc.next().toCharArray();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = tmp[j] - '0';
             }
         }
 
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<n;j++) {
-                if (!visited[i][j] && map[i][j]==1) {
-                    pq.add(bfs(i,j));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    dfs(i, j);
+                    pq.add(cnt);
+                    cnt = 1;
+                    // pq.add(bfs(i,j));
                 }
             }
         }
@@ -38,32 +41,48 @@ class Main {
             System.out.println(pq.poll());
         }
 
-
         // for (int i=0;i<n;i++) {
-        //     for (int j=0;j<n;j++) {
-        //         System.out.print(map[i][j]+" ");
-        //     }
-        //     System.out.println()
+        // for (int j=0;j<n;j++) {
+        // System.out.print(map[i][j]+" ");
+        // }
+        // System.out.println()
         // }
     }
 
-    private static int bfs(int x,int y) {
-        Queue<int[]> q=new LinkedList<>();
-        q.add(new int[]{x,y});
-        visited[x][y]=true;
-        int cnt=1;
+    private static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    cnt++;
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+
+    private static int bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { x, y });
+        visited[x][y] = true;
+        int cnt = 1;
 
         while (!q.isEmpty()) {
-            int[] cur=q.poll();
+            int[] cur = q.poll();
 
-            for (int i=0;i<4;i++) {
-                int nx=cur[0]+dx[i];
-                int ny=cur[1]+dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
 
-                if (nx>=0 && ny>=0 && nx<n && ny<n) {
-                    if (!visited[nx][ny] && map[nx][ny]==1) {
-                        visited[nx][ny]=true;
-                        q.add(new int[]{nx,ny});
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        q.add(new int[] { nx, ny });
                         cnt++;
                     }
                 }
